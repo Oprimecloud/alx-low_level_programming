859733150d51e5c9c32e6ae01db544f0c8026d91
Solagbade Abdulmalik O <100474947+Oprimecloud@users.noreply.github.com>
2023-02-28 00:10:46 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /**
  * reset_to_98 - Resets the value fo a variable to 98
  * @n: a pointer dereference for changing the value of the variable
- * Author: Sammykingx
+ * Author: Optimist
  * Return 0 success.
 */
 
